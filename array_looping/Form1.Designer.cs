0e752951d9a04109c99cfef522a80e827c0a1c6c
fikrihelmi17 <fikrihelmi17@gmail.com>
2020-07-03 13:46:18 +0700

    Initial commit

---
+++ array_looping/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace array_looping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtJumlah = new System.Windows.Forms.TextBox();
+            this.txtKode = new System.Windows.Forms.TextBox();
+            this.txtStok = new System.Windows.Forms.TextBox();
+            this.txtTahun = new System.Windows.Forms.TextBox();
+            this.txtPenerbit = new System.Windows.Forms.TextBox();
+            this.txtPengarang = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnProses = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmbJenis = new System.Windows.Forms.ComboBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jumlah Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Stok Buku";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tahun Terbit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Penerbit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Pengarang";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Jenis Buku";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Nama Buku";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Kode Buku";
+            // 
+            // txtJumlah
+            // 
+            this.txtJumlah.Location = new System.Drawing.Point(121, 5);
+            this.txtJumlah.Name = "txtJumlah";
+            this.txtJumlah.Size = new System.Drawing.Size(111, 20);
+            this.txtJumlah.TabIndex = 8;
+            // 
+            // txtKode
+            // 
+            this.txtKode.Location = new System.Drawing.Point(121, 39);
+            this.txtKode.Name = "txtKode";
+            this.txtKode.Size = new System.Drawing.Size(111, 20);
+            this.txtKode.TabIndex = 9;
+            // 
+            // txtStok
+            // 
+            this.txtStok.Location = new System.Drawing.Point(121, 215);
+            this.txtStok.Name = "txtStok";
+            this.txtStok.Size = new System.Drawing.Size(111, 20);
+            this.txtStok.TabIndex = 10;
+            // 
+            // txtTahun
+            // 
+            this.txtTahun.Location = new System.Drawing.Point(121, 190);
+            this.txtTahun.Name = "txtTahun";
+            this.txtTahun.Size = new System.Drawing.Size(111, 20);
+            this.txtTahun.TabIndex = 11;
+            // 
+            // txtPenerbit
+            // 
+            this.txtPenerbit.Location = new System.Drawing.Point(121, 163);
+            this.txtPenerbit.Name = "txtPenerbit";
+            this.txtPenerbit.Size = new System.Drawing.Size(111, 20);
+            this.txtPenerbit.TabIndex = 12;
+            // 
+            // txtPengarang
+            // 
+            this.txtPengarang.Location = new System.Drawing.Point(121, 134);
+            this.txtPengarang.Name = "txtPengarang";
+            this.txtPengarang.Size = new System.Drawing.Size(111, 20);
+            this.txtPengarang.TabIndex = 13;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(121, 68);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(111, 20);
+            this.txtNama.TabIndex = 15;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(238, 5);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(51, 21);
+            this.btnCreate.TabIndex = 16;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(121, 241);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(51, 21);
+            this.btnSimpan.TabIndex = 17;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(181, 241);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(51, 21);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnProses
+            // 
+            this.btnProses.Location = new System.Drawing.Point(121, 268);
+            this.btnProses.Name = "btnProses";
+            this.btnProses.Size = new System.Drawing.Size(51, 21);
+            this.btnProses.TabIndex = 19;
+            this.btnProses.Text = "Proses";
+            this.btnProses.UseVisualStyleBackColor = true;
+            this.btnProses.Click += new System.EventHandler(this.btnProses_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(181, 268);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(51, 21);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 307);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(396, 150);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // cmbJenis
+            // 
+            this.cmbJenis.FormattingEnabled = true;
+            this.cmbJenis.Items.AddRange(new object[] {
+            "Pendidikan",
+            "Romance",
+            "Sci-Fi",
+            "Kriminal",
+            "Lain-Lain"});
+            this.cmbJenis.Location = new System.Drawing.Point(121, 100);
+            this.cmbJenis.Name = "cmbJenis";
+            this.cmbJenis.Size = new System.Drawing.Size(111, 21);
+            this.cmbJenis.TabIndex = 22;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "No";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 25;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Kode";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 35;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Nama";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 75;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Pengarang";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 75;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Stok";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 40;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Tanggal Dimasukkan Data";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 489);
+            this.Controls.Add(this.cmbJenis);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnProses);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtPengarang);
+            this.Controls.Add(this.txtPenerbit);
+            this.Controls.Add(this.txtTahun);
+            this.Controls.Add(this.txtStok);
+            this.Controls.Add(this.txtKode);
+            this.Controls.Add(this.txtJumlah);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtJumlah;
+        private System.Windows.Forms.TextBox txtKode;
+        private System.Windows.Forms.TextBox txtStok;
+        private System.Windows.Forms.TextBox txtTahun;
+        private System.Windows.Forms.TextBox txtPenerbit;
+        private System.Windows.Forms.TextBox txtPengarang;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnProses;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbJenis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ array_looping/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace array_looping
+{
+    public partial class Form1 : Form
+    {
+        int k = 0;
+        int jumlah;
+        string[] kode;
+        string[] nama;
+        string[] jenis;
+        string[] pengarang;
+        string[] penerbit;
+        string[] tahunTerbit;
+        int[] stok;
+        int i = 0;
+
+        //double na;
+        //double nm;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            jumlah = Convert.ToInt32(txtJumlah.Text);
+
+            kode = new string[jumlah + k];
+            nama = new string[jumlah + k];
+            jenis = new string[jumlah + k];
+            pengarang = new string[jumlah + k];
+            penerbit = new string[jumlah + k];
+            tahunTerbit = new string[jumlah + k];
+            stok = new int[jumlah + k];
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            
+
+            if (i <= jumlah - 1)
+            {
+                kode[i + k] = txtKode.Text;
+                nama[i + k] = txtNama.Text;
+                jenis[i + k] = cmbJenis.Text;
+                pengarang[i + k] = txtPengarang.Text;
+                penerbit[i + k] = txtPenerbit.Text;
+                tahunTerbit[i + k] = txtTahun.Text;
+                stok[i + k] = Convert.ToInt32(txtStok.Text);
+
+                i++;
+            }
+            else
+            {
+                MessageBox.Show("Data tidak boleh lebih dari " + jumlah);
+            }
+
+            txtKode.Clear();
+            txtNama.Clear();
+            cmbJenis.ResetText();
+            txtPengarang.Clear();
+            txtPenerbit.Clear();
+            txtKode.Focus();
+            txtTahun.Text = "";
+            txtStok.Text = "";
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            
+            for (int j = 0; j < jumlah; j++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[j + k].Cells[0].Value = j + 1 + k;
+                dataGridView1.Rows[j + k].Cells[1].Value = kode[j + k];
+                dataGridView1.Rows[j + k].Cells[2].Value = nama[j + k];
+                dataGridView1.Rows[j + k].Cells[3].Value = pengarang[j + k];   
+                dataGridView1.Rows[j + k].Cells[4].Value = stok[j + k];
+                dataGridView1.Rows[j + k].Cells[5].Value = DateTime.Now.ToString("dd/MMMM/yyyy");
+            }
+            k = k + jumlah;
+            i = 0;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtKode.Clear();
+            txtNama.Clear();
+            cmbJenis.ResetText();
+            txtPengarang.Clear();
+            txtPenerbit.Clear();
+            txtKode.Focus();
+            txtTahun.Text = "";
+            txtStok.Text = "";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+    }
+}
